ec3046076d3e37dfcd773972431f12d89ce1db4b
Daniel Bedarf <Daniel.Bedarf@googlemail.com>
2012-10-21 21:40:49 +0200

    add link and ads function call

--- scr/Lib.AlternateDataStreams/ADSManagement.cpp
+++ scr/Lib.AlternateDataStreams/ADSManagement.cpp
@@ -5,4 +5,5 @@
 void HalloWelt()
 {
 	OutputDebugString(_T("Hallo Welt"));
+	//http://msdn.microsoft.com/en-us/library/windows/desktop/aa364424%28v=vs.85%29.aspx
 }
--- scr/Lib.AlternateDataStreams/ADSManagement.h
+++ scr/Lib.AlternateDataStreams/ADSManagement.h
@@ -1,1 +1,8 @@
+#include <vector>
+#include <shellapi.h>
+#include <tchar.h>
+
+using namespace std;
 void HalloWelt();
+
+vector<string> getAlternateDataStreams(TCHAR*  path);
